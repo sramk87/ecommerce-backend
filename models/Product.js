911b5c120b055d8a3bd568d98dfef92c025b0946
sramk87 <sramkissoon87@gmail.com>
2022-11-30 19:41:53 -0500

    models updated

--- models/Product.js
+++ models/Product.js
@@ -1,16 +1,43 @@
-// import important parts of sequelize library
 const { Model, DataTypes } = require('sequelize');
-// import our database connection from config.js
 const sequelize = require('../config/connection');
 
-// Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
 
-// set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    id: {
+     type: DataTypes.INTEGER,
+     allowNull: false,
+     primaryKey: true,
+     autoIncrement: true
+   },
+   category_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'category',
+      key: 'id'
+    }
   },
+  stock: {
+    type: DataTypes.INTEGER,
+    allowNull: false, 
+    defaultValue: 10,
+    validate: {
+      isNumeric: true
+    }
+  },
+   price: {
+     type: DataTypes.DECIMAL, 
+     allowNull: false,
+     validate: {
+       isDecimal: true
+     }
+   }
+ },
   {
     sequelize,
     timestamps: false,
